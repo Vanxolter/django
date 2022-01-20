a974c9afbbe69cd5f1b211fad09670db23212f06
Vanxolter <Vanxolter1@mail.ru>
2022-01-20 21:47:13 +0300

    add test and made minor changes

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -9,23 +9,26 @@ logger = logging.getLogger(__name__)
 
 def posts_index(request):
     result = ""
-    user = User.objects.get(username=request.GET.get("author", "maksim"))
     author_name = request.GET.get("author", "maksim")
     for x in Post.objects.filter(author__username=author_name).order_by("-id"):
-        result += f"<div style='border: 1px solid black'>"
+        result += f"<div style='border: 2px solid black'>"
         result += f"<h1>{x.title} #{x.id}</h1>"
         result += f"<div>{x.text}</div>"
-        result += f"</div>"
+        result += f"</div></br>"
     return HttpResponse(result)
 
 
 # Вводим в url строку запрос в виде: title/?title='наш титл'
 def search_by_title(request):
+    result = ""
     value = request.GET.get("title")
     logger.info(f'Пользователь {request.user} ввел Get-запросом данный текст: {value}')
-    single_post = Post.objects.filter(title = value).first()
-    return HttpResponse(f" <h4>Титл '{value}' создан пользователем: {single_post.author}</h4> \n "
-                        f"и имеет текст '{single_post.text}', время создания {single_post.created_at}")
+    for x in Post.objects.filter(title=value).order_by("-id"):
+        result += f"<div style='border: 2px solid black'>"
+        result += f"<h1>{x.title} #{x.id}</h1>"
+        result += f"<div>{x.text}</div>"
+        result += f"</div></br>"
+    return HttpResponse(result)
 
 
 # Работает кривовато, выводит первый пост, пробовал подставлять .all, при выводе выдает ошибку так как type(post_by_user) = class, засовывал в for и все безуспешно
