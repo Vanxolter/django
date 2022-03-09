efac1d4967878001e3953757348d52f930bdcd93
Vanxolter <Vanxolter1@mail.ru>
2022-03-09 21:23:20 +0300

    Herok Csrf trusted origins 1

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -27,7 +27,7 @@ DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["https://vanx-django.herokuapp.com/", "http://127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://vanx-django.herokuapp.com", "http://127.0.0.1"]
 
 # Application definition
 
